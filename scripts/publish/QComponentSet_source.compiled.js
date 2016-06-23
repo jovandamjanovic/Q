2bb4d0ae2a7d4235a3bdae72e3e98d2408873335
jovandamjanovic <jovandamjanovic@gmail.com>
2016-06-23 03:54:24 +0200

    Init Push
    First push of prepared libraries. QComponentSet and the rest prepared
    for testing.

---
+++ scripts/publish/QComponentSet_source.compiled.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var xmlToJson = function xmlToJson(xml) {
+    // Create the return object
+    var obj = {};
+    if (xml.nodeType == 1) {
+        // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3 && xml.nodeValue !== "") {
+        // text
+        obj = xml.nodeValue;
+    }
+    // do children
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof obj[nodeName] == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof obj[nodeName].push == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
+};
+
+var QComponent = function () {
+    function QComponent(name, width, height) {
+        _classCallCheck(this, QComponent);
+
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    _createClass(QComponent, [{
+        key: "render",
+        value: function render() {
+            return this.name;
+        }
+    }]);
+
+    return QComponent;
+}();
+
+var QComponentSet = function () {
+    function QComponentSet(catalogueName, metaDataDoc) {
+        var _this = this;
+
+        _classCallCheck(this, QComponentSet);
+
+        this.catalogueName = catalogueName;
+        return new Promise(function (resolve) {
+            var that = _this;
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    that.metaData = xmlToJson(xhttp.responseXML);
+                    resolve(that);
+                }
+            };
+            xhttp.open("GET", metaDataDoc, true);
+            xhttp.send();
+        });
+    }
+
+    _createClass(QComponentSet, [{
+        key: "newInstance",
+        value: function newInstance(type) {
+            var filteredComponent = this.metaData.ComponentCatalogue.Component.filter(function (current) {
+                if (current.Id["#text"] === type) {
+                    return current;
+                }
+            });
+            if (filteredComponent.length > 0) {
+                var componentMetaData = filteredComponent[0];
+                return new QComponent(componentMetaData.Name["#text"], Number.parseInt(componentMetaData.Width["#text"]), Number.parseInt(componentMetaData.Height["#text"]));
+            }
+        }
+    }]);
+
+    return QComponentSet;
+}();
---
+++ scripts/source/QComponentSet_source.js
@@ -0,0 +1,76 @@
+let xmlToJson = function (xml) {
+    // Create the return object
+    var obj = {};
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3 && xml.nodeValue !== "") { // text
+        obj = xml.nodeValue;
+    }
+    // do children
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof (obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof (obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
+};
+
+class QComponent {
+    constructor(name, width, height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    render() {
+        return this.name;
+    }
+}
+
+class QComponentSet {
+    constructor(catalogueName, metaDataDoc) {
+        this.catalogueName = catalogueName;
+        return new Promise((resolve) => {
+            var that = this;
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    that.metaData = xmlToJson(xhttp.responseXML);
+                    resolve(that);
+                }
+            };
+            xhttp.open("GET", metaDataDoc, true);
+            xhttp.send();
+        });
+    }
+
+    newInstance(type) {
+        var filteredComponent = this.metaData.ComponentCatalogue.Component.filter(function (current) {
+            if (current.Id["#text"] === type) {
+                return current;
+            }
+        });
+        if (filteredComponent.length > 0) {
+            var componentMetaData = filteredComponent[0];
+            return new QComponent(componentMetaData.Name["#text"], Number.parseInt(componentMetaData.Width["#text"]), Number.parseInt(componentMetaData.Height["#text"]));
+        }
+    }
+}
